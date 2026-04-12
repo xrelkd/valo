95be023bd894d3138a2346bde47331c0c015e1d2
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-04-12 17:26:30 +0800

    build(nix): add `--no-tests pass` for `cargo-nextest-all`

--- devshell/cargo-ext.nix
+++ devshell/cargo-ext.nix
@@ -61,6 +61,6 @@ in
     cargo --version
     rustc --version
     cargo nextest --version
-    cargo nextest run --workspace --no-fail-fast --no-capture "$@"
+    cargo nextest run --workspace --no-fail-fast --no-capture --no-tests pass "$@"
   '';
 }
